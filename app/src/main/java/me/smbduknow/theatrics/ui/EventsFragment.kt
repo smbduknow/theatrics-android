4c8c2cbc1edf108dcc637b5d033b709c39ef56da
smbduknow <smbduknow@gmail.com>
2016-07-15 01:13:37 +0300

    refreshing presenterLoader + callbacks

--- app/src/main/java/me/smbduknow/theatrics/ui/EventsFragment.kt
+++ app/src/main/java/me/smbduknow/theatrics/ui/EventsFragment.kt
@@ -1,32 +1,32 @@
 package me.smbduknow.theatrics.ui
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v4.app.LoaderManager
-import android.support.v4.content.Loader
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_feed.*
 import me.smbduknow.theatrics.R
+import me.smbduknow.theatrics.mvp.FeedMvpPresenter
 import me.smbduknow.theatrics.mvp.FeedMvpView
+import me.smbduknow.theatrics.mvp.PresenterLoaderCallback
 import me.smbduknow.theatrics.presenter.FeedPresenter
-import me.smbduknow.theatrics.presenter.FeedPresenterLoader
 import me.smbduknow.theatrics.presenter.FeedState
 import me.smbduknow.theatrics.ui.commons.InfiniteScrollListener
 import me.smbduknow.theatrics.ui.commons.inflate
 import me.smbduknow.theatrics.ui.model.UiEvent
 
 
-class EventsFragment : Fragment(), FeedMvpView, LoaderManager.LoaderCallbacks<FeedPresenter> {
+class EventsFragment : Fragment(), FeedMvpView {
 
     private val feedLoader by lazy { feed_loader }
+    private val feedSwipeRefresh by lazy { feed_swipe_refresh }
     private val feedList by lazy { feed_list }
     private val feedEmpty by lazy { feed_empty }
 
-    private var presenter: FeedPresenter? = null
+    private var presenter: FeedMvpPresenter? = null
 
     private var state = FeedState(0, 0, 0)
 
@@ -37,15 +37,14 @@ class EventsFragment : Fragment(), FeedMvpView, LoaderManager.LoaderCallbacks<Fe
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        feedSwipeRefresh.setOnRefreshListener { presenter?.requestNext(true) }
+
         val linearLayout = LinearLayoutManager(context)
         feedList.apply {
             setHasFixedSize(true)
             layoutManager = linearLayout
             clearOnScrollListeners()
-            addOnScrollListener(InfiniteScrollListener(linearLayout, {
-                Snackbar.make(view as View, "${state.listPage}" as CharSequence, Snackbar.LENGTH_SHORT).show()
-                presenter?.requestNext()
-            } ))
+            addOnScrollListener(InfiniteScrollListener(linearLayout, { presenter?.requestNext() } ))
             adapter = EventsAdapter()
         }
     }
@@ -60,23 +59,11 @@ class EventsFragment : Fragment(), FeedMvpView, LoaderManager.LoaderCallbacks<Fe
             addItems(state.listItems)
         }
 
-//      LoaderCallbacks as an object, so no hint regarding loader will be leak to the subclasses.
-        loaderManager.initLoader(101, null, this)
-    }
-
-
-    override fun onCreateLoader(id: Int, args: Bundle?): Loader<FeedPresenter> {
-        return FeedPresenterLoader(context)
-    }
-
-    override fun onLoadFinished(loader: Loader<FeedPresenter>, presenter: FeedPresenter) {
-        if (this@EventsFragment.presenter == null) {
-            this@EventsFragment.presenter = presenter
-        }
-    }
-
-    override fun onLoaderReset(loader: Loader<FeedPresenter>) {
-        this@EventsFragment.presenter = null
+        loaderManager.initLoader(101, null, PresenterLoaderCallback(context,
+                { FeedPresenter() },
+                { if(presenter == null) presenter = it },
+                { presenter = null }
+        ))
     }
 
 
@@ -99,27 +86,25 @@ class EventsFragment : Fragment(), FeedMvpView, LoaderManager.LoaderCallbacks<Fe
         super.onSaveInstanceState(outState)
         outState.putParcelable("state", state.apply {
             listItems = (feedList.adapter as EventsAdapter).getNews()
-            listPosition = (feedList.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
         })
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
-
     override fun showLoader(visible: Boolean) {
+        feedSwipeRefresh.isRefreshing = true
         feedLoader.visibility = View.VISIBLE
         feedList.visibility = View.GONE
         feedEmpty.visibility = View.GONE
     }
 
     override fun showFeed(visible: Boolean) {
+        feedSwipeRefresh.isRefreshing = false
         feedLoader.visibility = View.GONE
         feedList.visibility = View.VISIBLE
         feedEmpty.visibility = View.GONE
     }
 
     override fun showEmptyView(visible: Boolean) {
+        feedSwipeRefresh.isRefreshing = false
         feedLoader.visibility = View.GONE
         feedList.visibility = View.GONE
         feedEmpty.visibility = View.VISIBLE
