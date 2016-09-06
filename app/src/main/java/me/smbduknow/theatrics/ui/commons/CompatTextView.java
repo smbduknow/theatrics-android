073a271306d42b9781722682765cd861a1a41af5
smbduknow <smbduknow@gmail.com>
2016-09-06 15:10:16 +0300

    CompatTextView

---
+++ app/src/main/java/me/smbduknow/theatrics/ui/commons/CompatTextView.java
@@ -0,0 +1,60 @@
+package me.smbduknow.theatrics.ui.commons;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.AppCompatDrawableManager;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+import me.smbduknow.theatrics.R;
+
+public class CompatTextView extends AppCompatTextView {
+	public CompatTextView(Context context) {
+		super(context);
+		init(null);
+	}
+
+	public CompatTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(attrs);
+	}
+
+	public CompatTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init(attrs);
+	}
+
+	private void init(@Nullable AttributeSet attrs) {
+		if (attrs != null) {
+			Context context = getContext();
+			TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CompatTextView);
+
+			// Obtain DrawableManager used to pull Drawables safely, and check if we're in RTL
+			AppCompatDrawableManager dm = AppCompatDrawableManager.get();
+			boolean rtl = ViewCompat.getLayoutDirection(this) == ViewCompat.LAYOUT_DIRECTION_RTL;
+
+			// Grab the compat drawable resources from the XML
+			int startDrawableRes = a.getResourceId(R.styleable.CompatTextView_compatDrawableStart, 0);
+			int topDrawableRes = a.getResourceId(R.styleable.CompatTextView_compatDrawableTop, 0);
+			int endDrawableRes = a.getResourceId(R.styleable.CompatTextView_compatDrawableEnd, 0);
+			int bottomDrawableRes = a.getResourceId(R.styleable.CompatTextView_compatDrawableBottom, 0);
+
+			// Load the used drawables, falling back to whatever may be set in an "android:" namespace attribute
+			Drawable[] currentDrawables = getCompoundDrawables();
+			Drawable start = startDrawableRes != 0 ? dm.getDrawable(context, startDrawableRes) : currentDrawables[0];
+			Drawable end = endDrawableRes != 0 ? dm.getDrawable(context, endDrawableRes) : currentDrawables[1];
+			Drawable top = topDrawableRes != 0 ? dm.getDrawable(context, topDrawableRes) : currentDrawables[2];
+			Drawable bottom = bottomDrawableRes != 0 ? dm.getDrawable(context, bottomDrawableRes) : currentDrawables[3];
+
+			// Account for RTL and apply the compound Drawables
+			Drawable left = rtl ? end : start;
+			Drawable right = rtl ? start : end;
+			setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+
+			a.recycle();
+		}
+	}
+}
